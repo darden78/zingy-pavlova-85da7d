409df485fce100cc38c4df08f4df818837d39eb0
darden78 <151368603+darden78@users.noreply.github.com>
2026-08-10 22:46:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fantastica-v474-device-online";
+const CACHE="fantastica-v475-assignment-undo";
 const ASSETS=["./","./index.html","./telefono.html","./verify.html","./pc.html","./config.js","./manifest.webmanifest","./manifest-phone.webmanifest","./manifest-pc.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
