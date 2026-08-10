bb6d6185913cdb73cae1c486e263525080d24761
darden78 <151368603+darden78@users.noreply.github.com>
2026-08-10 20:24:02 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = "https://evjwadffhsyzltnnzgqk.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5JZ7VvIiadcSkAvjpFZZxw_xgHC5ga5";
-export const PUBLIC_BASE_URL = window.location.origin;
+export const PUBLIC_BASE_URL = "https://darden78.github.io/zingy-pavlova-85da7d";
 export const LEGACY_LEAGUE_ID = "00000000-0000-4000-8000-000000000001";
