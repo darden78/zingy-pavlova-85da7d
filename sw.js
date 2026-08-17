5179f527b9387077be62f0d8590c45e9622b4763
darden78 <151368603+darden78@users.noreply.github.com>
2026-08-17 15:28:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fantastica-v514-pc-launcher-20260817-5";
+const CACHE="fantastica-v514-pc-roster-swipe-20260817-6";
 const ASSETS=["./","./index.html","./telefono.html","./gioca.html","./gioca-pc.html","./verify.html","./installa.html","./qr-installa.png","./pc.html","./spettatore.html","./config.js","./manifest.webmanifest","./manifest-phone.webmanifest","./manifest-pc.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./installa.html"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
