83e19bcdfe1eb1329a0560b496d9c0e8ed689b2d
darden78 <151368603+darden78@users.noreply.github.com>
2026-09-03 13:57:54 +0200

    Add files via upload

---
+++ test-prestazioni/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://nbsxpjwivhnyiqtaxehk.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_zjTz3AvLnTOOzqEJ8QrQPw_Swqb2T3F";
+export const PUBLIC_BASE_URL = "https://darden78.github.io/zingy-pavlova-85da7d/test-prestazioni";
+export const LEGACY_LEAGUE_ID = "00000000-0000-4000-8000-000000000001";
---
+++ test-prestazioni/sw.js
@@ -0,0 +1,5 @@
+const CACHE="fantastica-test-prestazioni-v560-20260903-1";
+const ASSETS=["./","./index.html","./telefono.html","./gioca.html","./gioca-pc.html","./verify.html","./installa.html","./qr-installa.png","./pc.html","./spettatore.html","./config.js","./manifest.webmanifest","./manifest-phone.webmanifest","./manifest-pc.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./timer_visible_double_ping.wav","./installa.html"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
