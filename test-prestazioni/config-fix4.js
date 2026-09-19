8b7be79e232af0cf5da5065df98fb466f753d8fc
darden78 <151368603+darden78@users.noreply.github.com>
2026-09-19 18:51:30 +0200

    Delete test-prestazioni directory

--- test-prestazioni/config-fix4.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL="https://nbsxpjwivhnyiqtaxehk.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_zjTz3AvLnTOOzqEJ8QrQPw_Swqb2T3F";
-export const PUBLIC_BASE_URL=window.location.origin;
